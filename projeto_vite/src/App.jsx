1c619039ecfae07ade16a6ad4892676d9274aefe
Carlos <carlosbsantos.cs@gmail.com>
2024-09-28 14:08:54 -0300

    'Projeto'

---
+++ projeto_vite/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Index from './components/Index'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+
+
+
+        
+        <Index />
+       
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ projeto_vite/src/components/Index.jsx
@@ -0,0 +1,18 @@
+
+const Index = () => {
+  
+return (
+
+    <div>
+
+        <h1>Página Inicial</h1>
+        <p>Esta é a página inicial do sistema.</p>
+
+    </div>
+
+    
+)
+
+}
+
+export default Index;
